bc6fc3787f38d03c27e54c406d85815f05327514
lapis-zero09 <shandy1124gaff@gmail.com>
2018-03-02 21:25:23 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,165 +1,3 @@
-debug = 1;
-
-function MagicBG(cvs, textureUrl, options) {
-
-    var self = this,
-        paused = true,
-        rafid;
-
-    self.isOK = false;
-    self.camera = null;
-    self.scene = null;
-    self.renderer = null;
-    self.texture = null;
-    self.lights = [];
-
-    self.defaults = {
-        accuracy: 0.4,
-        speed: 0.0015,
-
-        camera: {
-            perspective: 30,
-            position: { x: 0, y: 0.5, z: 10 },
-            lookAt: { x: 0, y: 0, z: 0 }
-        },
-        fog: {
-            enabled: true,
-            color: 0xFFFFFF,
-            density: .18,
-        },
-        tube: {
-            radius1: 1,
-            radius2: 2.5,
-            height: 25,
-            segments: 40,
-        },
-        lights: [
-            { pointLight: true, color: 0xd2e4ee, intensity: 20, position: { x: 0, y: 0, z: 0 } },
-            { pointLight: false, color: 0x278aef, intensity: 4, position: { x: -1, y: 1, z: 0 } },
-            { pointLight: false, color: 0xa3bbff, intensity: 5, position: { x: -1, y: -0.7, z: 0 } },
-        ],
-        renderCallback: function (sky) {
-            sky.camera.rotation.z = Math.PI - Math.sin(Date.now() / 1000) / 5;
-        }
-    };
-
-    self.isPaused = function () {
-        return paused;
-    };
-
-    function extend(destination, source) {
-        for (var property in source) {
-            if (typeof source[property] === 'object')
-                destination[property] = extend(destination[property], source[property]);
-            else
-                destination[property] = source[property];
-        }
-        return destination;
-    }
-
-    function addEvent(elem, type, eventHandle) {
-        if (elem == null || elem == undefined) return;
-        if (elem.addEventListener) {
-            elem.addEventListener(type, eventHandle, false);
-        } else if (elem.attachEvent) {
-            elem.attachEvent("on" + type, eventHandle);
-        } else {
-            elem["on" + type] = eventHandle;
-        }
-    };
-
-    function OnResizeCVS() {
-        self.camera.aspect = window.innerWidth / window.innerHeight;
-        self.camera.updateProjectionMatrix();
-        self.renderer.setSize(window.innerWidth * self.options.accuracy, window.innerHeight * self.options.accuracy);
-    }
-
-    function init() {
-        self.options = extend(self.defaults, options);
-
-        if (!!!window.WebGLRenderingContext) return false;
-
-        try {
-            self.renderer = new THREE.WebGLRenderer({ canvas: cvs });
-
-            // SCENE
-            self.scene = new THREE.Scene();
-            if (self.options.fog.enabled)
-                self.scene.fog = new THREE.FogExp2(self.options.fog.color, self.options.fog.density);
-
-            // CAMERA
-            self.camera = new THREE.PerspectiveCamera(self.options.camera.perspective, window.innerWidth / window.innerHeight, .1, 1000);
-            self.camera.position.set(self.options.camera.position.x, self.options.camera.position.y, self.options.camera.position.z);
-            self.camera.lookAt(new THREE.Vector3(self.options.camera.lookAt.x, self.options.camera.lookAt.y, self.options.camera.lookAt.z));
-            self.scene.add(self.camera);
-
-            // ON WINDOW RESIZE
-            addEvent(window, 'resize', OnResizeCVS);
-            OnResizeCVS();
-
-            // LIGHTS
-            for (var i = 0; i < self.options.lights.length; i++) {
-                var light = self.options.lights[i];
-                var l = light.pointLight == true ? new THREE.PointLight(light.color, light.intensity) : new THREE.DirectionalLight(light.color, light.intensity);
-                l.position.set(light.position.x, light.position.y, light.position.z);
-                self.scene.add(l);
-                self.lights.push(l);
-            }
-
-            // TUBE
-            if(typeof textureUrl == 'string')
-              self.texture = THREE.ImageUtils.loadTexture();
-            else
-              self.texture = textureUrl;
-          
-            self.texture.wrapT = THREE.RepeatWrapping;
-            var material = new THREE.MeshLambertMaterial({
-                map: self.texture,
-                side: THREE.DoubleSide
-            }),
-            geometry = new THREE.CylinderGeometry(
-                self.options.tube.radius2,
-                self.options.tube.radius1,
-                self.options.tube.height,
-                self.options.tube.segments, 1, true),
-
-            mesh = new THREE.Mesh(geometry, material);
-            mesh.rotation.x = Math.PI / 2;
-            self.scene.add(mesh);
-        } catch (e) {
-            return false;
-        }
-        return true;
-    }
-    self.isOK = init();
-
-    function animate() {
-        rafid = requestAnimationFrame(animate);
-        self.texture.offset.y -= self.options.speed;
-
-        self.options.renderCallback(self);
-        self.renderer.render(self.scene, self.camera);
-    }
-
-    self.toggle = function (run) {
-        if (!self.isOK) return false;
-
-        if (run === undefined)
-            self.toggle(!paused);
-
-        else if (!!run && paused) {
-            paused = false;
-            animate();
-        }
-        else if (!!!run) {
-            paused = true;
-            cancelAnimationFrame(rafid);
-        }
-        return true;
-    }
-}
-
-
 (function ($) {
 
     $.fn.fitText = function (kompressor, options) {
@@ -192,6 +30,7 @@ function MagicBG(cvs, textureUrl, options) {
     };
 
 })(jQuery);
+
 
 (function ($) {
 
@@ -271,9 +110,10 @@ function MagicBG(cvs, textureUrl, options) {
 
 })(jQuery);
 
+
 (function () {
     $('#main').fitText(8);
-   
+
     Modernizr.addTest('preserve3d', function () {
 
         var prop = Modernizr.prefixed('transformStyle');
@@ -302,7 +142,7 @@ function MagicBG(cvs, textureUrl, options) {
         if (browsers[b][0] = navigator.userAgent.toLowerCase().indexOf(browsers[b][1]) >= 0)
             $('html').addClass(browsers[b][1]);
     }
-    
+
     var resize_seq = false;
     $(window).resize(function (e) {
         if (!resize_seq) {
@@ -312,47 +152,25 @@ function MagicBG(cvs, textureUrl, options) {
     });
 })();
 
-//-----------------------------------------
 
 (function () {
+    var $main = $('#main');
+    var $world = $main.find('.world');
 
-  // Cross origin texture error trick
-var image = document.getElementById('img'),
-    texture = new THREE.Texture( undefined, {} );
-texture.image = image;
-texture.needsUpdate = true;
+    $main.find('.viewport').mousemove(function (e) {
+        var Vertical = 4,
+            Horizontal = 2,
+            Yangle = -Vertical + (e.clientY / window.innerHeight) * 2 * Vertical,
+            Xangle = -Horizontal + (e.clientX / window.innerWidth) * 2 * Horizontal;
 
-var sky = new MagicBG($('#cvs1').get(0), texture, { accuracy: 0.3 }).toggle(true);
-  
-    var booting = true;
-  
-    function InitIntro() {
-        var $main = $('#main');
-        var $world = $main.find('.world');
+        $world.css('transform', 'rotateX( ' + -Yangle + 'deg) rotateY( ' + Xangle + 'deg)');
+    });
 
-        $main.find('.viewport').mousemove(function (e) {
-            var Vertical = 4,
-                Horizontal = 2,
-                Yangle = -Vertical + (e.clientY / window.innerHeight) * 2 * Vertical,
-                Xangle = -Horizontal + (e.clientX / window.innerWidth) * 2 * Horizontal;
+    setTimeout(function () {
+        $main.toggleClass('play');
+        $main.find('#skills-slider').yslider(['Python', 'Ruby', 'SQL', 'Java', 'JS'], { speed: 150, pause: 500, delay: 2600 });
+        $main.find('#like-slider').yslider(['Programming', 'Machine Learning', 'Security', 'Blockchain', 'good Food & Drink', 'Corgi'], { speed: 150, pause: 500, delay: 2600 });
 
-            $world.css('transform', 'rotateX( ' + -Yangle + 'deg) rotateY( ' + Xangle + 'deg)');
-        });
-
-        $main.find('.loader').toggleClass('hide');
-
-        setTimeout(function () {
-            if (!debug) $('html, body').scrollTop(0);
-            $main.toggleClass('play');
-            $main.find('#skills-slider').yslider(['.NET', 'C', 'C#', 'C++', 'JS', 'PHP', 'WPF'], { speed: 150, pause: 500, delay: 2600 });
-            $main.find('#like-slider').yslider(['Programming', 'UI Design', 'Skiing', 'Football', 'NLP', 'Squash'], { speed: 150, pause: 500, delay: 2600 });
-
-            booting = false;
-
-            $(window).resize();
-          $("#main" ).unbind( "click", InitIntro );
-        }, 300);
-    }
-
-  $('#main').click(InitIntro);
+        $(window).resize();
+    }, 300);
 })();
